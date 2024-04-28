e84554de78ba66350bb837cdf8989067a80fabc8
Shehab Emon <emonhasan20002000@gmail.com>
2024-04-28 22:03:32 +0100

    - added custom cursor

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,17 +1,20 @@
-import type { AppProps } from "next/app";
-import { Circles, Layout } from "@/components";
-import { ConfigProvider, ThemeProvider } from "@/providers";
-import "@/styles/globals.scss";
+import type { AppProps } from 'next/app';
+import { Circles, CustomCursor, Layout } from '@/components';
+import { ConfigProvider, ThemeProvider } from '@/providers';
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <ConfigProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        <Circles />
-      </ConfigProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <ConfigProvider>
+          <Layout>
+            <Component {...pageProps} />
+            <CustomCursor />
+          </Layout>
+          <Circles />
+        </ConfigProvider>
+      </ThemeProvider>
+    </>
   );
 }
