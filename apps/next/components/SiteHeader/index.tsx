9530b769df58c6be0e8729831933e4257f5d2c02
Shehab Emon <emonhasan20002000@gmail.com>
2024-04-28 15:01:49 +0100

    - added animation to site header elements

--- apps/next/components/SiteHeader/index.tsx
+++ apps/next/components/SiteHeader/index.tsx
@@ -1,16 +1,42 @@
+import React, { useRef } from 'react';
 import Link from 'next/link';
 import { useContext } from 'react';
 import { Container, NavDraw } from '@/components';
+import { useGSAP, gsap } from '@/lib';
 import { Context } from '@/contexts/Context';
 import { useTheme } from '@/providers';
 import styles from './styles.module.scss';
 
 export default function SiteHeader() {
+  const navListRef = useRef<HTMLUListElement>(null);
   const { isNavOpen, setIsNavOpen } = useContext(Context);
   const { theme, toggleTheme } = useTheme();
   const isDarkMode = theme === 'dark-theme';
   const date = new Date();
   const year = date.getFullYear();
+
+  useGSAP(
+    () => {
+      const navItems = gsap.utils.toArray(navListRef?.current?.children!);
+      gsap.fromTo(
+        navItems,
+        {
+          y: 15,
+          opacity: 0
+        },
+        {
+          y: 0,
+          opacity: 1,
+          delay: 0.2,
+          stagger: {
+            each: 0.3,
+            ease: 'power2.inOut'
+          }
+        }
+      );
+    },
+    { scope: navListRef }
+  );
 
   return (
     <header
@@ -20,13 +46,15 @@ export default function SiteHeader() {
     >
       <nav className={styles.nav}>
         <Container isFluid={false}>
-          <ul className={styles.navList}>
+          <ul className={styles.navList} ref={navListRef}>
             <li className={styles.navItem} onClick={toggleTheme}>
               <button>{isDarkMode ? 'light.' : 'dark.'}</button>
             </li>
+
             <li className={styles.navItem}>
               <Link href={`/`}>est. {year}</Link>
             </li>
+
             <li className={styles.navItem}>
               <button className={styles.burgerMenu} type={'button'} onClick={() => setIsNavOpen(!isNavOpen)}>
                 <div className={styles.menuIcon}>
