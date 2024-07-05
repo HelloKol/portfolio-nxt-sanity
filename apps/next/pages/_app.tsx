f79acdf9dae65be638703b58c99b64729683e2f8
Shehab Emon <emonhasan20002000@gmail.com>
2024-07-05 22:32:09 +0100

    - added condition to rendering app when ready

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,20 +1,25 @@
 import type { AppProps } from 'next/app';
+import { use, useEffect, useState } from 'react';
 import { Circles, CustomCursor, Layout } from '@/components';
 import { ConfigProvider, ThemeProvider } from '@/providers';
 import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  if (!isMounted) return null;
+
   return (
-    <>
-      <ThemeProvider>
-        <ConfigProvider>
-          <Layout>
-            <Component {...pageProps} />
-            <CustomCursor />
-          </Layout>
-          <Circles />
-        </ConfigProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <ConfigProvider>
+        <Layout>
+          <Component {...pageProps} />
+          <CustomCursor />
+        </Layout>
+        <Circles />
+      </ConfigProvider>
+    </ThemeProvider>
   );
 }
