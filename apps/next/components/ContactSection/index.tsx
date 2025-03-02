fe09f44dcd4a5178ee76e724750c0e341446907c
Shehab Emon <shehabhasan2020@gmail.com>
2025-03-02 22:34:56 +0000

    contact section redesigned per breakpoints

--- apps/next/components/ContactSection/index.tsx
+++ apps/next/components/ContactSection/index.tsx
@@ -64,41 +64,43 @@ export default function ContactSection() {
   return (
     <section
       id="contact"
-      className="contact relative mt-[200px] h-[500px] rounded-tl-[50px] rounded-tr-[50px] bg-[#18181A] pt-16 pb-20 xl:h-[600px] xl:pt-34"
+      className="contact relative mt-[200px] rounded-tl-[50px] rounded-tr-[50px] bg-[#18181A] pt-16 pb-10 md:h-[450px] md:pb-20 lg:h-[430px] xl:h-[530px] xl:pt-24"
     >
-      <Container>
+      <Container className="relative z-20 md:z-0">
         <div className="grid grid-cols-12 gap-10">
-          <div className="left-side col-span-12 space-y-6 md:col-span-6">
-            <h1 className="text-xl font-bold text-white">Socials</h1>
+          <div className="left-side order-2 col-span-12 space-y-6 md:order-1 md:col-span-6">
+            <h1 className="text-lg font-bold text-white md:text-xl">Socials</h1>
             <div className="social-links mt-6 text-4xl xl:text-5xl">
               {renderSocials()}
             </div>
           </div>
 
-          <div className="right-side col-span-12 space-y-6 text-right md:col-span-6">
-            <h1 className="text-xl font-bold text-white">Get in touch</h1>
+          <div className="right-side order-1 col-span-12 space-y-6 md:order-2 md:col-span-6 md:text-right">
+            <h1 className="text-lg font-bold text-white md:text-xl">
+              Get in touch
+            </h1>
             <Link
               href="https://x.com/0x_josh"
               target="_blank"
               rel="noopener noreferrer"
-              className="mt-6 ml-auto block w-fit text-4xl xl:text-5xl"
+              className="mt-6 block w-fit text-4xl md:ml-auto xl:text-5xl"
             >
               <span className="text-white">Info@shehab.uk</span>
             </Link>
-            <div className="navigation-links flex items-center justify-end gap-4 text-xl">
+            <div className="navigation-links flex items-center gap-4 text-lg md:justify-end md:text-xl">
               {renderNavigation()}
             </div>
           </div>
         </div>
 
         <div className="copyright mt-10">
-          <p className="text-xl text-white">
+          <p className="text-lg text-white md:text-xl">
             &copy; {new Date().getFullYear()} Shehab Emon
           </p>
         </div>
       </Container>
 
-      <div className="absolute top-1/2 left-1/2 z-10 h-[700px] w-[500px] -translate-x-1/2 -translate-y-1/2 xl:h-[900px] xl:w-[580px]">
+      <div className="absolute right-0 bottom-0 z-10 h-[370px] w-[300px] md:top-1/2 md:left-1/2 md:h-[500px] md:w-[400px] md:-translate-x-1/2 md:-translate-y-1/2 lg:h-[700px] lg:w-[500px] xl:h-[900px] xl:w-[580px]">
         <TiltedCard
           imageSrc="/image/david-full.png"
           rotateAmplitude={8}
